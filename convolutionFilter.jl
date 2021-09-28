6ab7a44f93d0a770a01c35e4570a94d61589711b
therode <theorodester@gmail.com>
2021-09-28 11:22:47 -0700

    started foundations to expand filter to do RGB and greyscale as well non-square filters

--- convolutionFilter.jl
+++ convolutionFilter.jl
@@ -29,32 +29,38 @@ function convolution_filter(image, filter)
 
     startTime = now() #get exact time on function start to later calculate time it took to run
 
-    out = zeros(size(image)) #create blank image array with same dimentions as input array 
+    if length(size(image)) == 3 && size(image)[1] == 3 #if the image array has three dimensions and the first dimension is 3, interpret it as a RGB image
 
-    #save row and column dimensions of image for use later
-    rowImageSize = size(image)[2]
-    columnImageSize = size(image)[3]
 
-    filterOverlap = Int(floor(size(filter)[1]/2)) #save the value of the "overlap" of the filter for later use
+        out = zeros(size(image)) #create blank image array with same dimentions as input array 
 
-    padWidth = Pad(0, filterOverlap, filterOverlap) #set pad amount for image to be the floor of half the kernel size. This should be equal to the amount of overlap of the kernel when looking at the edge of the image
+        #save row and column dimensions of image for use later
+        rowImageSize = size(image)[2]
+        columnImageSize = size(image)[3]
 
-    #citation for image padding: https://discourse.julialang.org/t/is-there-any-padding-function-available-to-pad-a-matrix-or-array/8521/3
-    image = padarray(image, padWidth) #pad input array using closest in bound pixel (yes this does differ from the python implementation, but I doubt this significantly affects performance?)
+        #find the row and column filter overlap
+        rowFilterOverlap = Int(floor(size(filter)[1]/2)) 
+        columnFilterOverlap = Int(floor(size(filter)[2]/2))
 
-    for i in 1:rowImageSize 
-        for j in 1:columnImageSize 
-            #take the dot product of the kernel and the section of the image the kernel overlaps (for each channel) and set it to the corresponding pixel in the output image
-            out[1, i, j] = dot(image[1, i-filterOverlap:i+filterOverlap, j-filterOverlap:j+filterOverlap], filter)
-            out[2, i, j] = dot(image[2, i-filterOverlap:i+filterOverlap, j-filterOverlap:j+filterOverlap], filter)
-            out[3, i, j] = dot(image[3, i-filterOverlap:i+filterOverlap, j-filterOverlap:j+filterOverlap], filter)
+        padWidth = Pad(0, rowFilterOverlap, columnFilterOverlap) #set pad amount for image to be the floor of half the kernel size. This should be equal to the amount of overlap of the kernel when looking at the edge of the image
+
+        #citation for image padding: https://discourse.julialang.org/t/is-there-any-padding-function-available-to-pad-a-matrix-or-array/8521/3
+        image = padarray(image, padWidth) #pad input array using closest in bound pixel (yes this does differ from the python implementation, but I doubt this significantly affects performance?)
+
+        for i in 1:rowImageSize 
+            for j in 1:columnImageSize 
+                #take the dot product of the kernel and the section of the image the kernel overlaps (for each channel) and set it to the corresponding pixel in the output image
+                out[1, i, j] = dot(image[1, i-rowFilterOverlap:i+columnFilterOverlap, j-rowFilterOverlap:j+columnFilterOverlap], filter)
+                out[2, i, j] = dot(image[2, i-rowFilterOverlap:i+columnFilterOverlap, j-rowFilterOverlap:j+columnFilterOverlap], filter)
+                out[3, i, j] = dot(image[3, i-rowFilterOverlap:i+columnFilterOverlap, j-rowFilterOverlap:j+columnFilterOverlap], filter)
+
+            end
 
         end
-
     end
 
     totalTime = now() - startTime #take different between current time and start time to get time function took to run
-    prinln("Filter ran on image in $totalTime") #display runtime in print statement
+    println("Filter ran on image in $totalTime") #display runtime in print statement
 
     return out
 
