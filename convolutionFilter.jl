0cc80ea0cdb1f3142354dc33dfb116ab6e5df99d
therode <theorodester@gmail.com>
2021-10-01 10:29:52 -0700

    converted filter to take in either RGB or greyscale images

--- convolutionFilter.jl
+++ convolutionFilter.jl
@@ -57,6 +57,32 @@ function convolution_filter(image, filter)
             end
 
         end
+    
+    elseif length(size(image)) == 2 #if the image only has two dimensions, assume greyscale... Almost same as RGB, however only 1 channel
+        out = zeros(size(image)) #create blank image array with same dimentions as input array 
+
+        #save row and column dimensions of image for use later
+        rowImageSize = size(image)[1]
+        columnImageSize = size(image)[2]
+
+        #find the row and column filter overlap
+        rowFilterOverlap = Int(floor(size(filter)[1]/2)) 
+        columnFilterOverlap = Int(floor(size(filter)[2]/2))
+
+        padWidth = Pad(rowFilterOverlap, columnFilterOverlap) #set pad amount for image to be the floor of half the kernel size. This should be equal to the amount of overlap of the kernel when looking at the edge of the image
+
+        #citation for image padding: https://discourse.julialang.org/t/is-there-any-padding-function-available-to-pad-a-matrix-or-array/8521/3
+        image = padarray(image, padWidth) #pad input array using closest in bound pixel (yes this does differ from the python implementation, but I doubt this significantly affects performance?)
+
+        for i in 1:rowImageSize 
+            for j in 1:columnImageSize 
+                #take the dot product of the filter and corisponding area in image
+                out[i, j] = dot(image[i-rowFilterOverlap:i+columnFilterOverlap, j-rowFilterOverlap:j+columnFilterOverlap], filter)
+
+            end
+
+        end
+
     end
 
     totalTime = now() - startTime #take different between current time and start time to get time function took to run
